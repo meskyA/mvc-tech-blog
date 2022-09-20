acd2a5042beaaae299fefd606c9442d81a5b8d6a
meskyA <mesky2015@gmail.com>
2022-09-20 15:39:35 -0700

    worked on more router codes

--- controllers/api/postRouter.js
+++ controllers/api/postRouter.js
@@ -2,31 +2,32 @@ const router = require('express').Router();
 const { Post } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
-// CREATE POST
+// Create New Post
 router.post('/', withAuth, async (req, res) => {
   const body = req.body;
     console.log(body);
   try {
     const newPost = await Post.create({ ...body, userId: req.session.userId });
-    console.log("Here is the new post: ",  newPost);
+    console.log("This is the new post: ",  newPost);
     res.json(newPost);
      } catch (err) {
-       console.log('IT FAILED!', err);
+       console.log('ERROR!', err);
     res.status(500).json(err);
   }
 });
 
-// UPDATE POST
+// Update 
+
 router.put('/:id', withAuth, async (req, res) => {
   try {
-    console.log('here is the req.body', req.body);
-    const [affectedRows] = await Post.update(req.body, {
+    console.log('req.body', req.body);
+    const [updatedRows] = await Post.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
 
-    if (affectedRows > 0) {
+    if (updatededRows > 0) {
       res.status(200).end();
     } else {
       res.status(404).end();
@@ -36,16 +37,17 @@ router.put('/:id', withAuth, async (req, res) => {
   }
 });
 
-// DELETE POST
+// Delete/Destroy post
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const [affectedRows] = Post.destroy({
+    const [updatedRows] = Post.destroy({
       where: {
         id: req.params.id,
       },
     });
 
-    if (affectedRows > 0) {
+    if (updatedRows > 0) {
       res.status(200).end();
     } else {
       res.status(404).end();
---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { Post, User } = require('../models/');
+const withAuth = require('../utils/auth');
+
+// Posts dashboard
+router.get('/', withAuth, async (req, res) => {
+  try {
+  
+    const postData = await Post.findAll({
+      where:{"userId": req.session.userId},
+      include: [User]
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+console.log(posts);
+    res.render('allPosts', {
+      // specifies a different layout.
+      layout: 'dashboard',
+      posts,
+    });
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+// New post
+router.get('/new', withAuth, (req, res) => {
+  res.render('newPost', {
+    layout: 'dashboard',
+  });
+});
+
+// Edit a post
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    if (postData) {
+      // serializing the data
+      const post = postData.get({ plain: true });
+      console.log(post);
+      res.render('editPost', {
+        layout: 'dashboard',
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+module.exports = router;

d319d082e902dcfd79eb38c595b15f9be24771cd
meskyA <mesky2015@gmail.com>
2022-09-21 18:46:34 -0700

    modifies server.js to connect middleware by changing route file structue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const routes = require('.');
+const routes = require('./controllers/homeRoutes');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 

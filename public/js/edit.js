71c0c25ec00a5b6342c7aff96f6599549e25b7fe
meskyA <mesky2015@gmail.com>
2022-10-10 01:21:28 -0700

    works on edit

--- public/js/edit.js
+++ public/js/edit.js
@@ -3,7 +3,7 @@ async function editFormHandler(event) {
 
   const postTitle = document.querySelector('input[name="post-title"]').value.trim();
   // const postContent = document.querySelector('input[name="content"]').value.trim();
-  const postContent = document.querySelector('textarea[name="post-text"]').value.trim();
+  const postContent = document.querySelector('post-text[name="post-text"]').value.trim();
   postContent,
   console.log(postTitle);
   // console.log(postContent);
@@ -33,5 +33,5 @@ async function editFormHandler(event) {
 }
 
 document
-.querySelector('.edit-post-form')
+.querySelector('#edit-post-form')
 .addEventListener('submit', editFormHandler);
